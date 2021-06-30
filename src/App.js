71938a4537f774928d4c22e6d8380ac0589f8e95
unknown <marcos.barlotti@gmail.com>
2021-06-30 20:19:58 -0300

    Desafio 2 con dropdown

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import { div } from 'prelude-ls';
 import React from 'react';
 //COMPONENTS
 import Header from './components/header/Header';
+import Navbar from './components/navbar/Navbar';
+import Cart from './components/cart/Cart';
+
 
 class App extends React.Component{
   render() {
     return (
       <div>
+        <Navbar/>
         <Header/>
       </div>
     )
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,17 +1,11 @@
 import React from 'react'
 import "./Header.css";
-//COMPONENTES
-import Navbar from '../navbar/Navbar';
-import Cart from "../cart/Cart"
-import image from '../../images/cinetazasEscrito.png';
+
 
 
 function Header() {
     return (
         <header className="header">
-            <img className="logo" src={image} />
-            <Navbar />
-            <Cart />
         </header>
     )
 }
